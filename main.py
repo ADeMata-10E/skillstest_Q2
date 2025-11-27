0e902a2e332c104beea1366a2142c734729117eb
adm <demata.aa@obmontessori.edu.ph>
2025-11-27 08:58:03 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+from pyscript import display, document
+
+Band ={"Band"
+"Advisor:"  "Mr. Smith"
+"Meeting Time:" "Fridays at 3 PM"
+"Location:" "Music Room"
+"Amt. of Members:" "20"
+"Category: Non-academic"}
+
+Chess ={"Chess"
+"Advisor:"  "Ms. Johnson"
+"Meeting Time:" "Tuesdays and Thursdays at 4 PM"
+"Location:" "Auditorium"
+"Amt. of Members:" "15"
+"Category: Non-academic"}
+
+IntRelations = {"Social Studies"
+"Advisor:"  "Ms. Lee"
+"Meeting Time:" "Wednesdays at 2 PM"
+"Location:" "Room 204"
+"Amt. of Members:" "25"
+"Category: Academic"}
+
+Science = {"Science"
+"Advisor:"  "Dr. Brown"
+"Meeting Time:" "Mondays at 3 PM"
+"Location:" "Lab 1"
+"Amt. of Members:" "19"
+"Category: Academic"}
+
+Music = {"Art"
+       "Advisor:"  "Ms. Green"
+       "Meeting Time:" "Thursdays at 2 PM"
+       "Location:" "Music Room"
+       "Amt. of Members:" "22"
+       "Category: Non-academic"}
+
+#dictionary
+
+school_clubs = {
+    "band": Band,
+    "chess": Chess,
+    "international_relations": IntRelations,
+    "science": Science,
+    "music": Music,
+}
+
+
+
+#Dictionary display in the HTML element with id 'output'
+
+
+
+ 
+
+
+
+
+    
+
+
+
+   
